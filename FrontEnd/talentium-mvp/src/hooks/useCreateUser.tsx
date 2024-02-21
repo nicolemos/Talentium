1feaa04ffef452e91ed9c8654e051bbe26be919d
Brian Lagranda <brianlagranda@gmail.com>
2024-02-21 19:56:38 -0300

    Se implementó método para el Login del usuario

--- FrontEnd/talentium-mvp/src/hooks/useCreateUser.tsx
+++ FrontEnd/talentium-mvp/src/hooks/useCreateUser.tsx
@@ -1,14 +1,16 @@
-import { UserProps } from '../interfaces/RegisterFormTypes';
+import { UserProps } from "../interfaces/RegisterFormTypes";import { useAuth } from "../context/AuthContext";
 
 const useCreateUser = () => {
+    const auth = useAuth();
+
     const createUser = async (userData: UserProps): Promise<boolean> => {
-        const url = 'http://localhost:8080/usuarios';
+        const url = "http://localhost:8080/usuarios";
 
         try {
             const response = await fetch(url, {
-                method: 'POST',
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify(userData),
             });
@@ -19,12 +21,40 @@ const useCreateUser = () => {
                 return false; // Hubo un error al introducir los datos
             }
         } catch (error) {
-            console.error('Ocurrió un error:', error);
+            console.error("Ocurrió un error:", error);
             return false; // Hubo un error al realizar la solicitud
         }
     };
 
-    return { createUser };
+    const loginUser = async (credentials: {
+        email: string;
+        password: string;
+    }): Promise<boolean> => {
+        const loginUrl = "http://localhost:8080/login";
+
+        try {
+            const response = await fetch(loginUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(credentials),
+            });
+
+            if (response.ok) {
+                const user = await response.json();
+                auth.login(user);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (error) {
+            console.error("An error occurred:", error);
+            return false;
+        }
+    };
+
+    return { createUser, loginUser };
 };
 
 export default useCreateUser;
