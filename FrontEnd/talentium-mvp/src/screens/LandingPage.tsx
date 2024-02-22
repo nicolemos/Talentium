44c67d90b899dfb52a72f880909734b507c030af
Brian Lagranda <brianlagranda@gmail.com>
2024-02-22 15:54:43 -0300

    Se arregló error en conflicto merge

--- FrontEnd/talentium-mvp/src/screens/LandingPage.tsx
+++ FrontEnd/talentium-mvp/src/screens/LandingPage.tsx
@@ -1,4 +1,5 @@
-import DashboardCliente from "./DashboardCliente";import Footer from "./Footer";
+import DashboardCliente from "./DashboardCliente";
+import Footer from "./Footer";
 import Header from "./Header";
 import Hero from "./Hero";
 import Article from "../components/Article";
@@ -9,28 +10,6 @@ import { LoginForm } from "../interfaces/LoginForm";
 const LandingPage: React.FC<LoginForm> = () => {
     const { user } = useAuth();
 
-<<<<<<< HEAD
-  return (
-    <div className=" bg-gradient-to-b from-royal-blue-500  to-white
-    flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow">
-        {!email ? (
-          <section className=" body-font flex-grow">
-            <Hero />
-            <Article />
-          </section>
-        ) : (
-          <>
-            <DashboardCliente />
-          </>
-        )}
-      </main>
-      
-      <Footer />
-    </div>
-  );
-=======
     return (
         <div
             className=' bg-gradient-to-b from-royal-blue-500  to-white
@@ -53,7 +32,6 @@ const LandingPage: React.FC<LoginForm> = () => {
             <Footer />
         </div>
     );
->>>>>>> cbf3fbddabcdb4bb749eb1404865e5bd9b611677
 };
 
 export default LandingPage;
