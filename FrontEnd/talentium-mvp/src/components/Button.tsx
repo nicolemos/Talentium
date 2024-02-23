b9a18ab6d8caac6c4baa381db9fad5716c6ae8c7
Brian Lagranda <brianlagranda@gmail.com>
2024-02-23 17:45:02 -0300

    Se modificó tag por button y se agregó onClick

--- FrontEnd/talentium-mvp/src/components/Button.tsx
+++ FrontEnd/talentium-mvp/src/components/Button.tsx
@@ -1,18 +1,17 @@
+interface ButtonProps {    children: string;
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
 
-  interface ButtonProps {
-    content: string;
-    href?: string;
-  }
-  
-  const GradientButton: React.FC<ButtonProps> = ({ content, href }) => {
+const CustomButton: React.FC<ButtonProps> = ({ children, onClick }) => {
     return (
-        <a
-            href={href}
-            className="transition duration-200 ease-in-out cursor-pointer bg-gradient-to-b from-white/60 to-white/50 hover:shadow-lg hover:bg-white p-[0.3rem] rounded-xl text-sm border border-solid "
+        <button
+            type='submit'
+            className='transition duration-200 ease-in-out cursor-pointer bg-gradient-to-b from-white/60 to-white/50 hover:shadow-lg hover:bg-white p-[0.3rem] rounded-xl text-sm border border-solid '
+            onClick={onClick}
         >
-            {content}
-        </a>
+            {children}
+        </button>
     );
-} 
-  
-export default GradientButton;
+};
+
+export default CustomButton;
