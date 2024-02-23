ac7db5a2b9b560b290cecb40c5e10ad736b51999
Brian Lagranda <brianlagranda@gmail.com>
2024-02-23 17:51:26 -0300

    Se refactorizó el renderizado del DashboardCliente ya que no se hace más en la LandingPage

--- FrontEnd/talentium-mvp/src/screens/LandingPage.tsx
+++ FrontEnd/talentium-mvp/src/screens/LandingPage.tsx
@@ -1,26 +1,17 @@
-import DashboardCliente from './DashboardCliente';
 import Footer from './Footer';
 import Header from './Header';
 import Hero from './Hero';
 import Article from '../components/Article';
-import { useAuth } from '../context/AuthContext';
 
-import { LoginForm } from '../interfaces/LoginForm';
-
-const LandingPage: React.FC<LoginForm> = () => {
-    const { user } = useAuth();
-    if (user?.email) {
-        return <DashboardCliente />;
-    }
-
+const LandingPage: React.FC = () => {
     return (
         <div
-            className="bg-gradient-to-b from-royal-blue-500  to-white
-    flex flex-col min-h-screen"
+            className='bg-gradient-to-b from-royal-blue-500  to-white
+    flex flex-col min-h-screen'
         >
             <Header />
-            <main className="flex-grow">
-                <section className="flex-grow">
+            <main className='flex-grow'>
+                <section className='flex-grow'>
                     <Hero />
                     <Article />
                 </section>
