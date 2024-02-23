1671e742b1e4559d49e208171d2b6d02300012e2
Brian Lagranda <brianlagranda@gmail.com>
2024-02-23 17:15:29 -0300

    Se agregó ruta de DashboardCliente

--- FrontEnd/talentium-mvp/src/routers/AppRouter.tsx
+++ FrontEnd/talentium-mvp/src/routers/AppRouter.tsx
@@ -6,35 +6,44 @@ import LandingPage from '../screens/LandingPage';
 import BasicRegistrationForm from '../components/BasicRegistrationForm';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-//import DashboardCliente from '../screens/DashboardCliente';
+import DashboardRouter from '../routers/DashboardRouter';
 
 export const AppRouter: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/Login" element={<Login />} />
-        <Route
-          path="/Register"
-          element={
-            <div className="flex bg-gradient-to-b from-royal-blue-500 to-white h-screen justify-center items-center p-4">
-              <ToastContainer />
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<LandingPage />} />
+                <Route path='/' element={<DashboardRouter />} />
+                <Route path='/Login' element={<Login />} />
+                <Route
+                    path='/Register'
+                    element={
+                        <div className='flex bg-gradient-to-b from-royal-blue-500 to-white h-screen justify-center items-center p-4'>
+                            <ToastContainer />
 
-              <BasicRegistrationForm name="" surname="" mail="" password="" />
-            </div>
-          }
-        />
-        <Route path="/*" element={<PageNotFound />} />
-        <Route path="/acerca-de" element={<h1>ACA VA INFO DE ACERCA DE</h1>} />
-        <Route
-          path="/preguntas-frecuentes"
-          element={<h1>ACA VAN LAS PREGUNTAS FRECUENTES</h1>}
-        />
-        <Route
-          path="/terminos-y-condiciones"
-          element={<h1>ACA VAN LOS TERMINOS Y CONDICIONES</h1>}
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+                            <BasicRegistrationForm
+                                name=''
+                                surname=''
+                                mail=''
+                                password=''
+                            />
+                        </div>
+                    }
+                />
+                <Route path='/*' element={<PageNotFound />} />
+                <Route
+                    path='/acerca-de'
+                    element={<h1>ACA VA INFO DE ACERCA DE</h1>}
+                />
+                <Route
+                    path='/preguntas-frecuentes'
+                    element={<h1>ACA VAN LAS PREGUNTAS FRECUENTES</h1>}
+                />
+                <Route
+                    path='/terminos-y-condiciones'
+                    element={<h1>ACA VAN LOS TERMINOS Y CONDICIONES</h1>}
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 };
