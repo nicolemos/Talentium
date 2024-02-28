32db5914fe703ab0f9da822064b5f5fe9b4ab1c6
ariocal <126732667+ariocal@users.noreply.github.com>
2024-02-28 10:11:59 -0500

    modificando userDTO

--- BackEnd/homeservice/src/main/java/cohorte16/homeservice/dtos/UserDTO.java
+++ BackEnd/homeservice/src/main/java/cohorte16/homeservice/dtos/UserDTO.java
@@ -1,7 +1,17 @@
 package cohorte16.homeservice.dtos;
 
+import cohorte16.homeservice.models.User;
+
 public record UserDTO (Long id,
                        String email,
-                       String password,
                        String avatar){
+
+
+    public UserDTO(User user) {
+
+        this(   user.getId(),
+                user.getEmail(),
+                null
+    );
+    }
 }
