4600a3d14e7291a322b7804bc5d68582ec2e84d7
lionel staricoff <staricofflionel@hotmail.com>
2024-03-02 12:31:46 -0500

    agregando la vista para crear ordenes

---
+++ FrontEnd/ventanas_en_html_y_css/vista_crear_orden/js/Ordenes.js
@@ -0,0 +1,37 @@
+export class Ordenes {
+    constructor(dato) {
+        this.datos = dato;
+        this.textarea;
+        this.orden = {
+            "description": this.textarea ?? "nueva orden test",
+            "cliente_id": 15
+        }
+    }
+
+    crearOrden() {
+        const div = document.createElement('div');
+        div.id = "CrearOrden";
+
+        const textarea = document.createElement('textarea');
+        textarea.name = "descripcion";
+        textarea.placeholder = "orden";
+        textarea.cols = "30";
+        textarea.rows = "8";
+        textarea.innerText = this.textarea ?? "";
+
+        const button = document.createElement('button');
+        button.innerText = "Crear";
+        button.addEventListener('click', () => {
+            // console.log(this.datos);
+
+            this.orden.description = document.querySelector('textarea').value;
+            this.textarea = console.log(this.orden);
+        })
+        div.append(textarea, button);
+        return div;
+    }
+    agregarAlFront() {
+        const main = document.querySelector('#front');
+        main.appendChild(this.crearOrden());
+    }
+}
---
+++ FrontEnd/ventanas_en_html_y_css/vista_crear_orden/js/main.js
@@ -0,0 +1,11 @@
+import {datos} from './Datos.js';
+import {Ordenes} from './Ordenes.js';
+
+const body = document.querySelector('body');
+
+//console.log(datos.cliente());
+
+//console.log(datos.profesional());
+
+const nuevaOrden = new Ordenes(datos.cliente());
+nuevaOrden.agregarAlFront();
