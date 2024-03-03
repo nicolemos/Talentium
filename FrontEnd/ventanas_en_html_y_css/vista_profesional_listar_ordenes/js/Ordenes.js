39a2898c85f3e336a65de2e74dd66b3936e8cd30
lionel staricoff <staricofflionel@hotmail.com>
2024-03-03 00:29:27 -0500

    agregando la vista para aceptar una orden

---
+++ FrontEnd/ventanas_en_html_y_css/vista_profesional_listar_ordenes/js/Ordenes.js
@@ -0,0 +1,66 @@
+export class Ordenes {
+    constructor(datoOrden, profesionalDto, divPadre) {
+        this.datos = datoOrden;
+        this.textarea;
+        this.divPadre = divPadre ?? '#front';
+        this.comentario;
+        this.description = datoOrden.description;
+        this.precio;
+        this.profesional = profesionalDto;
+        this.orden = {
+            "id": 1,
+            "profecional": this.profesional.id,
+            "precio": NaN,
+            "comentarios": this.comentario,
+            "orderstatus": "PENDIENTE"
+
+        }
+    }
+
+    crearOrden() {
+        const div = document.createElement('div');
+        div.id = "CrearOrden";
+
+        const textarea = document.createElement('textarea');
+        textarea.name = "descripcion";
+        textarea.placeholder = "comentario";
+        textarea.cols = "30";
+        textarea.rows = "8";
+        textarea.innerText = this.textarea ?? "";
+
+        const precio = document.createElement('input');
+        precio.type = "number";
+        precio.placeholder = '$$$';
+
+        const button = document.createElement('button');
+        button.innerText = "Crear";
+        button.addEventListener('click', () => {
+
+            this.orden.comentarios = document.querySelector('textarea').value;
+            this.orden.precio = document.querySelector('input').value
+
+            console.log(this.orden);
+
+        });
+
+        const buttonX = document.createElement('button');
+        buttonX.innerText = "X";
+        buttonX.addEventListener('click', () => {
+            const padre = div.parentNode;
+            padre.removeChild(div);
+        });
+
+        const descripcionOrden = document.createElement('div');
+        descripcionOrden.innerText = this.description;
+        descripcionOrden.addEventListener('click', ()=>{
+alert('descripcion');
+        });
+
+            div.append(buttonX, textarea, precio, button, descripcionOrden);
+        return div;
+    }
+    agregarAlFront() {
+        const main = document.querySelector(this.divPadre);
+        main.appendChild(this.crearOrden());
+    }
+}
---
+++ FrontEnd/ventanas_en_html_y_css/vista_profesional_listar_ordenes/js/main.js
@@ -0,0 +1,11 @@
+import {datos} from './Datos.js';
+import {Ordenes} from './Ordenes.js';
+
+const profecional1 = datos.orden();
+const nuevaOrden = new Ordenes(datos.orden(), datos.profesional());
+nuevaOrden.agregarAlFront();
+nuevaOrden.agregarAlFront();
+nuevaOrden.agregarAlFront();
+
+//alert(profecional1.description)
+
