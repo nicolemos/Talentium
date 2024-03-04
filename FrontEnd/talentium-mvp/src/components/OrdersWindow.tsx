0f18e2b0b4376bae42756c108caeeb07b1688546
AngelAlvarezC <144290231+AngelAlvarezC@users.noreply.github.com>
2024-03-04 18:25:32 -0300

    correccion

--- FrontEnd/talentium-mvp/src/components/OrdersWindow.tsx
+++ FrontEnd/talentium-mvp/src/components/OrdersWindow.tsx
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
 
-const OrdersWindowClient = () => {
+const OrdersWindow = () => {
     return (
         <div className='flex max-w-[800px] flex-row flex-wrap items-center justify-center gap-4 p-4'>
-            {/* Primer enlace con texto */}
+
             <Link to='/CreateOrderClient'>
                 <div className='flex h-40 w-60 items-center justify-center rounded-md bg-royal-blue-700 text-white shadow-md hover:bg-royal-blue-500'>
                     Crear Orden
                 </div>
             </Link>
-            {/* Si quieres agregar otro componente dentro de Link, simplemente colócalo dentro. Ejemplo con texto plano: */}
+            
             <Link to='/OrderListClient'>
                 <div className='flex h-40 w-60 items-center justify-center rounded-md bg-royal-blue-700 text-white shadow-md hover:bg-royal-blue-500'>
                     Lista de Ordenes
@@ -19,4 +19,4 @@ const OrdersWindowClient = () => {
     );
 };
 
-export default OrdersWindowClient;
+export default OrdersWindow;
