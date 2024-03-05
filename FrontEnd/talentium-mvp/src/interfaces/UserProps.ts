75c534303cc4a5b4c8cefa89bd419a59cc2f6b6d
AngelAlvarezC <144290231+AngelAlvarezC@users.noreply.github.com>
2024-03-05 10:42:53 -0300

    se agrega prefil de usuario para actualizar datos

---
+++ FrontEnd/talentium-mvp/src/interfaces/UserProps.ts
@@ -0,0 +1,17 @@
+export interface User {
+    name?: string;
+    email?: string;
+    photoUrl?: string;
+}
+
+export interface PerfilProps {
+    userId: string;
+}
+
+export interface UserData {
+    nombre: string;
+    apellido: string;
+    dni: string;
+    email: string;
+    direccion: string;
+}
