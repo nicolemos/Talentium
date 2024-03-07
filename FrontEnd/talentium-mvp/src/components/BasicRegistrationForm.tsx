90b5b1d2dbc39c35f1a5c669eb52a2202dcad479
Magaerv <marielaramirezvalle@gmail.com>
2024-03-06 23:30:14 -0300

    se corrige errores para el register con el back OK

--- FrontEnd/talentium-mvp/src/components/BasicRegistrationForm.tsx
+++ FrontEnd/talentium-mvp/src/components/BasicRegistrationForm.tsx
@@ -38,7 +38,7 @@ const BasicRegistrationForm: React.FC<BasicRegistrationFormProps> = () => {
                 toast.success('Te has registrado exitosamente!');
                 navigate('/dashboardcliente/inicio');
                 if (userCreated.id) {
-                    localStorage.setItem('userId', userCreated.id);
+                    localStorage.setItem('userId', JSON.stringify(userCreated.id));
                 }
             } else {
                 toast.error(
@@ -196,7 +196,7 @@ const BasicRegistrationForm: React.FC<BasicRegistrationFormProps> = () => {
                 >
                     Avatar (opcional):
                     <input
-                        type='file'
+                        type='text'
                         {...register('avatar')}
                         accept='image/*'
                         onChange={handleAvatarChange}
